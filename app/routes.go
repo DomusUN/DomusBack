b49bd91e9d436203c608c17dcf0bb9fb4ae9e059
Juan Pablo Carmona <jucarmonam@unal.edu.co>
2022-10-13 19:03:22 -0500

    first commit

---
+++ app/routes.go
@@ -0,0 +1,13 @@
+package app
+
+import "github.com/gin-gonic/gin"
+
+// TODO: Replace this with dependency injection.
+func InitDepenedencies() {
+
+}
+
+func InitRoutes(router *gin.Engine) error {
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package DomusBack
+
+import (
+	"DomusBack/app"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	app.InitDepenedencies() // MUST initializate dependencies, otherwise you'll get like 1M errors.
+
+	err := app.InitRoutes(router)
+	if err != nil {
+		return
+	}
+
+	errRouter := router.Run()
+	if errRouter != nil {
+		return
+	}
+}
